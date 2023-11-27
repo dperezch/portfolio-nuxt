baf7aedad2919684a0dabef4f28d280f975be2ce
dperezch <davarmando@gmail.com>
2023-11-27 20:03:18 -0300

    dark mode

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <h1 class="text-8xl">PAGE</h1>
+    <UButton @click="colorMode.value = 'dark'" class="mx-3">dark</UButton>
+    <UButton @click="colorMode.value = 'light'" class="mx-3">light</UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+</script>
+
+<style lang="postcss">
+body {
+  @apply bg-slate-50 dark:bg-gray-800
+}
+</style>
